ccbbc9aa7af4b6d1ea726f1e5a1de750c9418d8c
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-11-11 15:29:39 +0530

    add frame

--- MINIPROJECT/src/BrickBreaker/Main.java
+++ MINIPROJECT/src/BrickBreaker/Main.java
@@ -1,5 +1,17 @@
 package BrickBreaker;
 
+import javax.swing.JFrame;
+
 public class Main {
+	public static void main(String[]args) {
+		JFrame frame=new JFrame();
+        frame.setSize(800,700);
+        frame.setTitle("THE BREAKOUT GAME");
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        
+	}
 
 }
